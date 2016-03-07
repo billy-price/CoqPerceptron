f4ce3b067b84c83d96a9aa9300af5fc38bd51203
Charlie Murphy <tm507211@ohio.edu>
2016-03-06 19:19:51 -0500

    Cleans up benchmarks (you can run any or all of them), corrects data (was from older version)

--- Benchmarks/cpp/makeplotfiles.cpp
+++ Benchmarks/cpp/makeplotfiles.cpp
@@ -4,93 +4,104 @@
 using namespace std;
 
 int main(int argc, char ** argv){
-  ifstream times("times");
-  ifstream fuels("fuels");
+  ifstream fuels("output/fuels");
+  ifstream cpptimes("output/cpptimes");
+  ifstream hstimes("output/hstimes");
+  ifstream hsopttimes("output/hsopttimes");
 
-  ofstream vectors("vectors.plot");
-  for (int i = 0; i < 5; ++i){
+  ofstream vectors("plots/vectors.plot");
+  for (int i = 0; i < 6; ++i){
     int vec;
     string s;
-    times >> s >> vec;
+    cpptimes >> s >> vec;
+    hstimes >> s >> vec;
+    hsopttimes >> s >> vec;
     vectors << vec << '\t';
     fuels >> s >> vec;
     int min, f;
     char c;
-    double sec, timeCPP(0), timeOCAML(0), timeHS(0), fuel(0);
+    double sec, timeCPP(0), timeHS(0), timeHSOpt(0), fuel(0);
     for (int j = 0; j < 10; ++j){
-      times >> min >> c >> sec >> c; timeCPP += 60*min + sec;
-      times >> min >> c >> sec >> c; timeOCAML += 60*min + sec;
-      times >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      cpptimes >> min >> c >> sec >> c; timeCPP += 60*min + sec;
+      hstimes >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      hsopttimes >> min >> c >> sec >> c; timeHSOpt += 60*min + sec;
       fuels >> s >> f; fuel += f;
     }
-    fuel /= 10.0; timeCPP /= 10.0; timeOCAML /= 10.0; timeHS /= 10.0;
+    fuel /= 10.0; timeCPP /= 10.0; timeHS /= 10.0; timeHSOpt /= 10.0;
     vectors << fuel << '\t'
             << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/vec << '\t'
-            << timeOCAML << '\t' << timeOCAML/fuel << '\t' << timeOCAML/vec << '\t'
-            << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/vec << '\t' << endl;
+            << timeHSOpt << '\t' << timeHSOpt/fuel << '\t' << timeHSOpt/vec << '\t'
+            << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/vec << endl;
   }
 
-  for (int i = 0; i < 4; ++i){
+  for (int i = 0; i < 3; ++i){
     int vec;
     string s;
-    times >> s >> vec;
+    cpptimes >> s >> vec;
+    hsopttimes >> s >> vec;
     vectors << vec << '\t';
     fuels >> s >> vec;
     int min, f;
     char c;
-    double sec, timeCPP(0), fuel(0);
+    double sec, timeCPP(0), timeHSOpt(0), fuel(0);
     for (int j = 0; j < 10; ++j){
-      times >> min >> c >> sec >> c; timeCPP += 60*min + sec;
+      cpptimes >> min >> c >> sec >> c; timeCPP += 60*min + sec;
+      hsopttimes >> min >> c >> sec >> c; timeHSOpt += 60*min + sec;
       fuels >> s >> f; fuel += f;
     }
-    fuel /= 10.0; timeCPP /= 10.0;
+    fuel /= 10.0; timeCPP /= 10.0; timeHSOpt /= 10.0;
     vectors << fuel << '\t'
-            << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/vec << '\t' << endl;
+            << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/vec << '\t'
+            << timeHSOpt << '\t' << timeHSOpt/fuel << '\t' << timeHSOpt/vec << endl;
   } vectors.close();
 
-  ofstream features("features.plot");
+  ofstream features("plots/features.plot");
   for (int i = 0; i < 6; ++i){
     int feat;
     string s;
-    times >> s >> feat;
+    cpptimes >> s >> feat;
+    hstimes >> s >> feat;
+    hsopttimes >> s >> feat;
     features << feat << '\t';
     fuels >> s >> feat;
     int min, f;
     char c;
-    double sec, timeCPP(0), timeOCAML(0), timeHS(0), fuel(0);
+    double sec, timeCPP(0), timeHS(0), timeHSOpt(0), fuel(0);
     for (int j = 0; j < 10; ++j){
-      times >> min >> c >> sec >> c; timeCPP += 60*min + sec;
-      times >> min >> c >> sec >> c; timeOCAML += 60*min + sec;
-      times >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      cpptimes >> min >> c >> sec >> c; timeCPP += 60*min + sec;
+      hstimes >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      hsopttimes >> min >> c >> sec >> c; timeHSOpt += 60*min + sec;
       fuels >> s >> f; fuel += f;
     }
-    fuel /= 10.0; timeCPP /= 10.0; timeOCAML /= 10.0; timeHS /= 10.0;
+    fuel /= 10.0; timeCPP /= 10.0; timeHS /= 10.0; timeHSOpt /= 10.0;
     features << fuel << '\t'
              << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/feat << '\t'
-             << timeOCAML << '\t' << timeOCAML/fuel << '\t' << timeOCAML/feat << '\t'
+             << timeHSOpt << '\t' << timeHSOpt/fuel << '\t' << timeHSOpt/feat << '\t'
              << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/feat << endl;
   } features.close();
 
-  ofstream Z("Z.plot");
+  ofstream Z("plots/Z.plot");
   for (int i = 0; i < 8; ++i){
     int Zs;
     string s;
-    times >> s >> Zs;
+    cpptimes >> s >> Zs;
+    hstimes >> s >> Zs;
+    hsopttimes >> s >> Zs;
     Z << Zs << '\t';
     fuels >> s >> Zs;
     int min, f;
     char c;
-    double sec, timeCPP(0), timeOCAML(0), timeHS(0), fuel(0);
+    double sec, timeCPP(0), timeHS(0), timeHSOpt(0), fuel(0);
     for (int j = 0; j < 10; ++j){
-      times >> min >> c >> sec >> c; timeCPP += 60*min + sec;
-      times >> min >> c >> sec >> c; timeOCAML += 60*min + sec;
-      times >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      cpptimes >> min >> c >> sec >> c; timeCPP += 60*min + sec;
+      hstimes >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      hsopttimes >> min >> c >> sec >> c; timeHSOpt += 60*min + sec;
       fuels >> s >> f; fuel += f;
     }
-    fuel /= 10.0; timeCPP /= 10.0; timeOCAML /= 10.0; timeHS /= 10.0;
+    fuel /= 10.0; timeCPP /= 10.0; timeHS /= 10.0; timeHSOpt /= 10.0;
     Z << fuel << '\t'
       << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/Zs << '\t'
-      << timeOCAML << '\t' << timeOCAML/fuel << '\t' << timeOCAML/Zs << '\t'
+      << timeHSOpt << '\t' << timeHSOpt/fuel << '\t' << timeHSOpt/Zs << '\t'
       << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/Zs << endl;
   } Z.close();
   return 0;
