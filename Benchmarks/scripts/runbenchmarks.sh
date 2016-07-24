edbd0936d19f0029b9a4b1cc07242c627b6d7e82
Timothy Murphy <tm507211@ohio.edu>
2016-07-24 14:54:05 -0400

    Adds raw results of benchmarks on data/*

--- Benchmarks/scripts/runbenchmarks.sh
+++ Benchmarks/scripts/runbenchmarks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || ([ -n "$6" ] && [ -z "$7" ])
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || ([ -n "$5" ] && [ -z "$6" ])
 then
   this=`basename "$0"`
-  echo "Usage: $this <PROGRAM_TO_RUN> <PATH_TO_DATA> <PATH_TO_OUTPUT> [<FUEL_OUTPUT>] [--all_vectors] [<VALIDATOR> <VALIDATION_OUTPUT>]"
-  echo "Example: $this cpp/perceptron data/ output/cpp/ output/fuels --all_vectors hs/RunValidator output/cpp_valid.dat"
+  echo "Usage: $this <PROGRAM_TO_RUN> <PATH_TO_DATA> <PATH_TO_OUTPUT> [<FUEL_OUTPUT>] [<VALIDATOR> <VALIDATION_OUTPUT>]"
+  echo "Example: $this cpp/perceptron data/ output/cpp/ output/fuels hs/RunValidator output/cpp_valid.dat"
   exit 1
 fi
 
@@ -15,13 +15,7 @@ else
   fout="$4"
 fi
 
-if [ -n "$5" ] && [ "$5" == "--all_vectors" ]
-then
-  vec=(50 100 200 400 800 1600 3200)
-else
-  vec=(50 100 200 400 800 1600)
-fi
-
+vec=(50 100 200 400 800 1600 3200)
 feat=(100 200 400 800 1600 3200)
 Z=(100 200 400 800 1600 3200 6400 12800)
 
@@ -29,17 +23,17 @@ for i in ${vec[*]};
 do
   echo "vectors $i" >> "$fout"
   echo "vectors $i"
-  if [ -n "$6" ]
+  if [ -n "$5" ]
   then
     echo "vectors $i" >&2
-    echo "vectors $i" >> "$7"
+    echo "vectors $i" >> "$6"
   fi
   for j in `seq 1 10`;
   do
     TIMEFORMAT="%lR"; time ($1 < $2/vec${i}_$j.dat > ${3}/vec${i}_$j 2>> "$fout") 2>&1
-    if [ -n "$6" ]
+    if [ -n "$5" ]
     then
-      TIMEFORMAT="%lR"; time (cat $2/vec${i}_$j.dat ${3}/vec${i}_$j | $6 >> "$7")
+      TIMEFORMAT="%lR"; time (cat $2/vec${i}_$j.dat ${3}/vec${i}_$j | $5 >> "$6")
     fi
   done
 done
@@ -48,17 +42,17 @@ for i in ${feat[*]};
 do
   echo "features $i" >> "$fout"
   echo "features $i"
-  if [ -n "$6" ]
+  if [ -n "$5" ]
   then
     echo "features $i" >&2
-    echo "features $i" >> "$7"
+    echo "features $i" >> "$6"
   fi
   for j in `seq 1 10`;
   do
     TIMEFORMAT="%lR"; time ($1 < $2/feat${i}_$j.dat > ${3}/feat${i}_$j 2>> "fout") 2>&1
-    if [ -n "$6" ]
+    if [ -n "$5" ]
     then
-      TIMEFORMAT="%lR"; time (cat $2/feat${i}_$j.dat ${3}/feat${i}_$j | $6 >> "$7")
+      TIMEFORMAT="%lR"; time (cat $2/feat${i}_$j.dat ${3}/feat${i}_$j | $5 >> "$6")
     fi
   done
 done
@@ -67,17 +61,17 @@ for i in ${Z[*]};
 do
   echo "Z $i" >> "$fout"
   echo "Z $i"
-  if [ -n "$6" ]
+  if [ -n "$5" ]
   then
     echo "Z $i" >&2
-    echo "Z $i" >> "$7"
+    echo "Z $i" >> "$6"
   fi
   for j in `seq 1 10`;
   do
     TIMEFORMAT="%lR"; time ($1 < $2/Z${i}_$j.dat > ${3}/Z${i}_$j 2>> "$fout") 2>&1
-    if [ -n "$6" ]
+    if [ -n "$5" ]
     then
-      TIMEFORMAT="%lR"; time (cat $2/Z${i}_$j.dat ${3}/Z${i}_$j | $6 >> "$7")
+      TIMEFORMAT="%lR"; time (cat $2/Z${i}_$j.dat ${3}/Z${i}_$j | $5 >> "$6")
     fi
   done
 done
