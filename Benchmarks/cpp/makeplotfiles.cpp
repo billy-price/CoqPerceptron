660c6d625a5cc70da9ce9b8478b561f8eb89d905
Timothy Murphy <tm507211@ohio.edu>
2016-08-30 16:33:50 -0400

    updates plot images, ploting method, and hsopt (coq -> Rationals extraction) output

--- Benchmarks/cpp/makeplotfiles.cpp
+++ Benchmarks/cpp/makeplotfiles.cpp
@@ -8,6 +8,7 @@ int main(int argc, char ** argv){
   ifstream cpptimes("output/cpptimes");
   ifstream cppvaltimes("output/cpp_valid_times");
   ifstream hstimes("output/hstimes");
+  ifstream hsopttimes("output/hsopttimes");
 
   ofstream vectors("plots/vectors.plot");
   for (int i = 0; i < 7; ++i){
@@ -16,22 +17,25 @@ int main(int argc, char ** argv){
     cpptimes >> s >> vec;
     cppvaltimes >> s >> vec;
     hstimes >> s >> vec;
+    hsopttimes >> s >> vec;
     vectors << vec << '\t';
     fuels >> s >> vec;
     int min, f;
     char c;
-    double sec, timeCPP(0), timeCPPVal(0), timeHS(0), fuel(0);
+    double sec, timeCPP(0), timeCPPVal(0), timeHS(0), timeHSOpt(0), fuel(0);
     for (int j = 0; j < 10; ++j){
       cpptimes >> min >> c >> sec >> c; timeCPP += 60*min + sec;
       cppvaltimes >> min >> c >> sec >> c; timeCPPVal += 60*min + sec;
       hstimes >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      hsopttimes >> min >> c >> sec >> c; timeHSOpt += 60*min + sec;
       fuels >> s >> f; fuel += f;
     }
-    fuel /= 10.0; timeCPP /= 10.0; timeCPPVal /= 10.0; timeHS /= 10.0;
+    fuel /= 10.0; timeCPP /= 10.0; timeCPPVal /= 10.0; timeHS /= 10.0; timeHSOpt /= 10.0;
     vectors << fuel << '\t'
             << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/vec << '\t'
             << timeCPPVal << '\t' << timeCPPVal/fuel << '\t' << timeCPPVal/vec << '\t'
-            << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/vec << endl;
+            << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/vec << '\t'
+            << timeHSOpt << '\t' << timeHSOpt/fuel << '\t' << timeHSOpt/vec << endl;
   } vectors.close();
 
   ofstream features("plots/features.plot");
@@ -41,22 +45,25 @@ int main(int argc, char ** argv){
     cpptimes >> s >> feat;
     cppvaltimes >> s >> feat;
     hstimes >> s >> feat;
+    hsopttimes >> s >> feat;
     features << feat << '\t';
     fuels >> s >> feat;
     int min, f;
     char c;
-    double sec, timeCPP(0), timeCPPVal(0), timeHS(0), fuel(0);
+    double sec, timeCPP(0), timeCPPVal(0), timeHS(0), timeHSOpt(0), fuel(0);
     for (int j = 0; j < 10; ++j){
       cpptimes >> min >> c >> sec >> c; timeCPP += 60*min + sec;
       cppvaltimes >> min >> c >> sec >> c; timeCPPVal += 60*min + sec;
       hstimes >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      hsopttimes >> min >> c >> sec >> c; timeHSOpt += 60*min + sec;
       fuels >> s >> f; fuel += f;
     }
-    fuel /= 10.0; timeCPP /= 10.0; timeCPPVal /= 10.0; timeHS /= 10.0;
+    fuel /= 10.0; timeCPP /= 10.0; timeCPPVal /= 10.0; timeHS /= 10.0; timeHSOpt /= 10.0;
     features << fuel << '\t'
              << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/feat << '\t'
              << timeCPPVal << '\t' << timeCPPVal/fuel << '\t' << timeCPPVal/feat << '\t'
-             << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/feat << endl;
+             << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/feat << '\t'
+             << timeHSOpt << '\t' << timeHSOpt/fuel << '\t' << timeHSOpt/feat << endl;
   } features.close();
 
   ofstream Z("plots/Z.plot");
@@ -66,22 +73,25 @@ int main(int argc, char ** argv){
     cpptimes >> s >> Zs;
     cppvaltimes >> s >> Zs;
     hstimes >> s >> Zs;
+    hsopttimes >> s >> Zs;
     Z << Zs << '\t';
     fuels >> s >> Zs;
     int min, f;
     char c;
-    double sec, timeCPP(0), timeCPPVal(0), timeHS(0), fuel(0);
+    double sec, timeCPP(0), timeCPPVal(0), timeHS(0), timeHSOpt(0), fuel(0);
     for (int j = 0; j < 10; ++j){
       cpptimes >> min >> c >> sec >> c; timeCPP += 60*min + sec;
       cppvaltimes >> min >> c >> sec >> c; timeCPPVal += 60*min + sec;
       hstimes >> min >> c >> sec >> c; timeHS += 60*min + sec;
+      hsopttimes >> min >> c >> sec >> c; timeHSOpt += 60*min + sec;
       fuels >> s >> f; fuel += f;
     }
-    fuel /= 10.0; timeCPP /= 10.0; timeCPPVal /= 10.0; timeHS /= 10.0;
+    fuel /= 10.0; timeCPP /= 10.0; timeCPPVal /= 10.0; timeHS /= 10.0; timeHSOpt /= 10.0;
     Z << fuel << '\t'
       << timeCPP << '\t' << timeCPP/fuel << '\t' << timeCPP/Zs << '\t'
       << timeCPPVal << '\t' << timeCPPVal/fuel << '\t' << timeCPPVal/Zs << '\t'
-      << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/Zs << endl;
+      << timeHS << '\t' << timeHS/fuel << '\t' << timeHS/Zs << '\t'
+      << timeHSOpt << '\t' << timeHSOpt/fuel << '\t' << timeHSOpt/Zs << endl;
   } Z.close();
   return 0;
 }
