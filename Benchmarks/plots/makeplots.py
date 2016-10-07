02d66824f3f8d0d322ea5e2e180d691754491c21
Charlie Murphy <tcm3@cs.princeton.edu>
2016-10-07 14:35:14 -0400

    Forgot to add update plot files yesterday

--- Benchmarks/plots/makeplots.py
+++ Benchmarks/plots/makeplots.py
@@ -5,6 +5,8 @@ use('Agg')
 import matplotlib.pyplot as plot
 import numpy as np
 
+plot.rcParams.update({'font.size': 16.1})
+
 def plot_data(img_name, file_name, xlabel, yRange = None):
   figure = plot.figure()
   axis = figure.add_subplot(1, 1, 1)
@@ -34,9 +36,9 @@ def plot_data(img_name, file_name, xlabel, yRange = None):
   C = [x / y for (x, y) in zip(C, D)]
   E = [x / y for (x, y) in zip(E, D)]
 
-  a_bar = plot.bar(X-0.375, A, color = 'g', width = 0.25)
-  b_bar = plot.bar(X-0.125, B, color = 'b',width = 0.25)
-  c_bar = plot.bar(X-0.125, C, color = 'r', bottom = B, width = 0.25)
+  a_bar = plot.bar(X-0.375, A, color = '#00fa9a', width = 0.25, hatch='//')
+  b_bar = plot.bar(X-0.125, B, color = 'cyan',width = 0.25, hatch='xx')
+  c_bar = plot.bar(X-0.125, C, color = '#ee82ee', bottom = B, width = 0.25, hatch='*')
   e_bar = plot.bar(X+0.125, E, color = 'orange', width = 0.25)
 
   axis.axes.get_xaxis().set_ticklabels(N)
@@ -49,11 +51,11 @@ def plot_data(img_name, file_name, xlabel, yRange = None):
 
   plot.legend([a_bar, b_bar, c_bar, e_bar],
               ["Coq Perceptron", "C++ Perceptron", "Coq Validator", "CoqOpt"],
-              loc=0)
+              loc="upper center", bbox_to_anchor=(0.5, 1.125), ncol=2)
   plot.xlabel(xlabel)
   plot.ylabel("Time")
   plot.savefig(img_name)
 
-plot_data("images/vecotrs.png", "vectors.plot", "Number of Vectors")
-plot_data("images/features.png", "features.plot", "Number of Features", [0,3.5])
-plot_data("images/Z.png", "Z.plot", u'Precision Bound -- Q = a/Z, a ∈ [-Z, Z]', [0,3.5])
+plot_data("images/VectorsPlot.png", "vectors.plot", "Number of Vectors", [0,9])
+plot_data("images/FeaturePlot.png", "features.plot", "Number of Features", [0,3])
+plot_data("images/ZPlot.png", "Z.plot", u'Precision Bound -- Q = a/Z, a ∈ [-Z, Z]', [0,3])
